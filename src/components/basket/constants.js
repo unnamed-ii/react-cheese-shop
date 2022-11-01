89313106e4f5533aa2d03e1cc302875a5357ab0f
iIngashev <isaingashev@gmail.com>
2022-11-01 21:32:36 +0300

    added constants file and dynamic for basket component

---
+++ src/components/basket/constants.js
@@ -0,0 +1,4 @@
+export const selectedProductsData = [
+    {title: 'Форма для твердого сыра 1 кг - 40 отверстий', price: '440'},
+    {title: 'Форма для твердого сыра 1 кг - 40 отверстий', price: '440'},
+]
--- src/components/basket/index.jsx
+++ src/components/basket/index.jsx
@@ -1,25 +1,37 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './style.scss'
 import {Link} from "react-router-dom";
 import SelectedProductCard from '../basket/selected-product-card';
+import {selectedProductsData} from "./constants";
 
 const Basket = ({isActive}) => {
+    const [selectedProducts, setSelectedProducts] = useState(selectedProductsData);
+
+    const calculateSumOfProducts = () => {
+        let sum = 0;
+        for (let i = 0; i < selectedProducts.length; i++){
+            sum += +selectedProducts[i].price
+        }
+        return sum
+    }
+
     return (
         <div className={"basket " + (isActive && "active")}>
             <div className="basket__title">
-                корзина (5)
+                корзина ({selectedProducts.length})
                 <div className="fragment" />
             </div>
             <div className="basket__items">
-                <SelectedProductCard />
-                <SelectedProductCard />
+                {selectedProducts.map(i => (
+                    <SelectedProductCard title={i.title} price={i.price}/>
+                ))}
             </div>
             <div className="basket__total">
                 <div className="basket__total-amount">
-                    Товаров в корзине: <span>5шт</span>
+                    Товаров в корзине: <span>{selectedProducts.length}шт</span>
                 </div>
                 <div className="basket__total-price">
-                    Общая стоимость: <span>1680 руб.</span>
+                    Общая стоимость: <span>{calculateSumOfProducts()} руб.</span>
                 </div>
             </div>
             <div className="basket__btn">
