10c25daa14702feeb5d28825f415d92ef00b021f
iIngashev <isaingashev@gmail.com>
2022-11-03 00:24:08 +0300

    refactor cart component

--- src/components/card/index.jsx
+++ src/components/card/index.jsx
@@ -1,20 +1,19 @@
 import React from 'react';
 import './style.scss'
-
 import card from '../../images/card.png'
 
-const Card = () => {
+const Card = ({description='Мезофильная закваска Danisco CHOOZIT MM...', price='1300₽', discountPrice='1800₽'}) => {
     return (
         <div className="card">
             <img className="card__image" src={card} alt="" />
             <div className="card__description">
-                Мезофильная закваска Danisco CHOOZIT MM...
+                {description}
             </div>
             <div className="horizontal" />
             <div className="card__bottom">
                 <div className="card__price">
-                    1300₽/<span> 1 шт.</span>
-                    <div className="discount">1800₽</div>
+                    {price}/<span> 1 шт.</span>
+                    <div className="discount">{discountPrice}</div>
                 </div>
                 <button className="card__select-btn">
                     Добавить
