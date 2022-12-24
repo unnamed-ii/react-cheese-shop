4d41403359d5a26aa49c8a82236ac658cc4cc656
iIngashev <isaingashev@gmail.com>
2022-12-24 21:32:01 +0300

    updated styles

--- src/components/input-range/index.jsx
+++ src/components/input-range/index.jsx
@@ -14,7 +14,8 @@ const CustomInputRange = () => {
                 <input type="number" className="max" value={max} onChange={handleChangeMax}/>
             </div>
             <label>
-                <input type="range" min={0} max={max} onChange={handleChangeMin} value={min} step={1}/>
+                <input type="range" min={0} max={max} onChange={handleChangeMin} value={min} step={1}
+                       className="range-min"/>
                 <input type="range" min={min} max={10000} onChange={handleChangeMax} value={max} step={1}
                        className="range-max"/>
             </label>
