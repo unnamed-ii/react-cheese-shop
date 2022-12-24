3e295ad4c6b4eb872c495010c2b2edcb796ea5f8
iIngashev <isaingashev@gmail.com>
2022-12-24 20:01:50 +0300

    refactoring and adding styles

--- src/components/input-range/index.jsx
+++ src/components/input-range/index.jsx
@@ -1,23 +1,24 @@
 import React, {useState} from 'react';
+import './style.scss'
 
 const CustomInputRange = () => {
     const [min, setMin] = useState(0)
     const [max, setMax] = useState(10000)
-
-    const handleChangeMin = (e) => {
-        setMin(e.target.value)
-    }
-
-    const handleChangeMax = (e) => {
-        setMax(e.target.value)
-    }
+    const handleChangeMin = e => setMin(e.target.value)
+    const handleChangeMax = e => setMax(e.target.value)
 
     return (
-        <label>
-            {min}_{max}
-            <input type="range" min={0} max={max} onChange={handleChangeMin} value={min} step={1}/>
-            <input type="range" min={min} max={10000} onChange={handleChangeMax} value={max} step={1}/>
-        </label>
+        <div className="custom-range">
+            <div className="custom-range__handle-inputs">
+                <input type="number" className="min" value={min} onChange={handleChangeMin}/>
+                <input type="number" className="max" value={max} onChange={handleChangeMax}/>
+            </div>
+            <label>
+                <input type="range" min={0} max={max} onChange={handleChangeMin} value={min} step={1}/>
+                <input type="range" min={min} max={10000} onChange={handleChangeMax} value={max} step={1}
+                       className="range-max"/>
+            </label>
+        </div>
     );
 };
 
