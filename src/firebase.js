fa20e2f650e3771f2762a259e1633047384fbb1f
unnamed-ii <isaingashev@gmail.com>
2023-01-07 13:28:38 +0300

    integrated firebase

---
+++ src/firebase.js
@@ -0,0 +1,14 @@
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+
+const firebaseConfig = {
+    apiKey: "AIzaSyBUgtgqPN1PgpKpYqBfZel7HpuDDrJ4GHc",
+    authDomain: "cheese-shopp.firebaseapp.com",
+    projectId: "cheese-shopp",
+    storageBucket: "cheese-shopp.appspot.com",
+    messagingSenderId: "837477032784",
+    appId: "1:837477032784:web:d3e98e0fa7021721be0255"
+};
+
+const app = initializeApp(firebaseConfig);
+export const database = getFirestore(app);
--- src/store/index.js
+++ src/store/index.js
@@ -2,11 +2,13 @@ import {createStore, combineReducers} from "redux";
 import {clientsReducer} from "./syncReducers/clients";
 import {basketReducer} from "./syncReducers/basket";
 import {categoryFiltersReducer} from "./syncReducers/category-filters";
+import {isAuthorizedReducer} from "./syncReducers/is-auth";
 
 const rootReducer = combineReducers({
     clients: clientsReducer,
     basket: basketReducer,
     filters: categoryFiltersReducer,
+    isAuth: isAuthorizedReducer,
 })
 
 export const store = createStore(rootReducer)
