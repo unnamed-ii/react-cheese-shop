434fc93fb67614998133811406836a3a05a281dc
unnamed-ii <isaingashev@gmail.com>
2023-01-15 15:24:04 +0300

    integrated redux-thunk .2

--- src/store/index.js
+++ src/store/index.js
@@ -1,8 +1,9 @@
-import {createStore, combineReducers} from "redux";
+import {createStore, combineReducers, applyMiddleware} from "redux";
 import {clientsReducer} from "./syncReducers/clients";
 import {basketReducer} from "./syncReducers/basket";
 import {categoryFiltersReducer} from "./syncReducers/category-filters";
 import {userReducer} from "./syncReducers/user";
+import thunk from "redux-thunk";
 
 const rootReducer = combineReducers({
     clients: clientsReducer,
@@ -11,4 +12,4 @@ const rootReducer = combineReducers({
     user: userReducer,
 })
 
-export const store = createStore(rootReducer)
+export const store = createStore(rootReducer, applyMiddleware(thunk))
