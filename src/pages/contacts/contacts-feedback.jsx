82c33ed3fceb6837c1936061882f74c706e83858
unnamed-ii <isaingashev@gmail.com>
2023-08-13 00:36:07 +0300

    changed image with real map at contacts page

--- src/pages/contacts/contacts-feedback.jsx
+++ src/pages/contacts/contacts-feedback.jsx
@@ -18,7 +18,12 @@ const ContactsFeedback = () => {
                     <button>Отправить</button>
                 </form>
             </div>
-            <img src={map} alt="" className="contacts__feedback-map"/>
+            <iframe
+                src="https://www.google.com/maps/embed?pb=!1m16!1m12!1m3!1d560.3195353175951!2d37.448958213533736!3d55.82313142425582!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!2m1!1z0YPQu9C40YbQsCDQodCy0L7QsdC-0LTRiyDQm9Cg0J_RgNCw0LnQtCwg0JzQvtGB0LrQstCwLCDQoNC-0YHRgdC40Y8!5e0!3m2!1sru!2str!4v1691875935658!5m2!1sru!2str"
+                allowFullScreen=""
+                loading="lazy"
+                referrerPolicy="no-referrer-when-downgrade"
+                className="contacts__feedback-map"/>
         </div>
     );
 };
